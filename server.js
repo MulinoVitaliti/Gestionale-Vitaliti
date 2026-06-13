ad7397c04a88472063a839c6daa49c967627067e
MulinoVitaliti <mulino.vitaliti@gmail.com>
2026-06-13 12:50:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const { Pool } = require('pg');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ── DATABASE ──────────────────────────────────────────────────────────────
