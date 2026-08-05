c41fae304868174d0fe1b14a4ce50c1092df4e5a
MulinoVitaliti <mulino.vitaliti@gmail.com>
2026-08-05 19:14:07 +0200

    Update app-core.js

--- public/app-core.js
+++ public/app-core.js
@@ -77,6 +77,7 @@ const api = {
   get: url => fetch(url).then(r=>r.json()),
   post: (url,data) => fetch(url,{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify(data)}).then(r=>r.json()),
   put: (url,data) => fetch(url,{method:'PUT',headers:{'Content-Type':'application/json'},body:JSON.stringify(data)}).then(r=>r.json()),
+  patch: (url,data) => fetch(url,{method:'PATCH',headers:{'Content-Type':'application/json'},body:JSON.stringify(data)}).then(r=>r.json()),
   del: url => fetch(url,{method:'DELETE'}).then(r=>r.json()),
 };
 
